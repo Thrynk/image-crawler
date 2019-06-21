d0aafdad24b750ef7ac7d18afac5230fcbf41996
CARIN THOMAS (QUAYS240) <TCARIN01@neptune.dkcorp.net>
2019-06-21 11:47:04 +0100

    only 404 images messages displayed in console

--- index.js
+++ index.js
@@ -15,17 +15,26 @@ function testImgStatus(imgUrl){
             function (error, response, body) {
                 if(!error){
                     if (response && response.statusCode === 404) {
-                        reject("Not found : https://decathlon.co.uk/" + imgUrl);
+                        resolve({
+                            message: "Not found : https://decathlon.co.uk/" + imgUrl,
+                            status: 404
+                        });
                     }
                     else if(response && response.statusCode === 400){
-                        reject("Bad request : https://decathlon.co.uk/" + imgUrl);
+                        resolve({
+                            message: "Bad request : https://decathlon.co.uk/" + imgUrl,
+                            status: 400
+                        });
                     }
                     else{
-                        resolve("Image OK : https://decathlon.co.uk/" + imgUrl);
+                        resolve({
+                            message: "Image OK : https://decathlon.co.uk/" + imgUrl,
+                            status: 200
+                        });
                     }
                 }
                 else {
-                    reject(error);
+                    reject("Error request");
                 }
             }
         );
@@ -39,10 +48,12 @@ sitemap.fetch('https://www.decathlon.co.uk/content/sitemaps/NavigationSitemap.xm
         imgsUrlCrawler(sites.sites[i]).then(async function (imgUrls) {
             for (let j = 0; j < imgUrls.length; j++) {
                 if(imgUrls[j].match(/skins/) === null){
-                    await testImgStatus(imgUrls[j]).then(function(sentenceToLog){
-                        console.log(sentenceToLog);
+                    await testImgStatus(imgUrls[j]).then(function(response){
+                        if(response.status === 404){
+                            console.log(response.message);
+                        }
                     }).catch(function(error){
-                        console.log("error");
+                        console.log(error);
                     });
                 }
             }
