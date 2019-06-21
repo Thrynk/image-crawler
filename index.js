c565ea3c97b1cea8dab7cbbe48628afd20731689
CARIN THOMAS (QUAYS240) <TCARIN01@neptune.dkcorp.net>
2019-06-21 10:41:42 +0100

    test with 1500 timeout

--- index.js
+++ index.js
@@ -4,34 +4,41 @@ const request = require("request");
 
 var sitemap = new Sitemapper();
 
+var testImgStatus = function(imgUrl){
+    imgUrl = imgUrl.replace("../", "");
+    request(
+        "https://decathlon.co.uk/" + imgUrl,
+        {
+            originalHostHeaderName: 'Host'
+        },
+        function (error, response, body) {
+            if(!error){
+                if (response && response.statusCode === 404) {
+                    console.log("https://decathlon.co.uk/" + imgUrl);
+                }
+                else if(response && response.statusCode === 400){
+                    console.log("Bad request : https://decathlon.co.uk/" + imgUrl);
+                }
+                else{
+                    console.log("Image OK : https://decathlon.co.uk/" + imgUrl);
+                }
+            }
+            else {
+                console.log(error);
+            }
+        }
+    );
+};
+
 sitemap.fetch('https://www.decathlon.co.uk/content/sitemaps/NavigationSitemap.xml').then(function(sites) {
     /*sites.sites.forEach(function(site){*/
-    for(var i = 0; i < 3; i++){
+    for(let i = 0; i < 3; i++){
         imgsUrlCrawler(sites.sites[i]).then(function (imgUrls) {
             imgUrls.forEach(function (imgUrl) {
-                imgUrl = imgUrl.replace("../", "");
-                request(
-                    "https://decathlon.co.uk/" + imgUrl,
-                    {
-                        originalHostHeaderName: 'Host'
-                    },
-                    function (error, response, body) {
-                        if(!error){
-                            if (response && response.statusCode === 404) {
-                                console.log("https://decathlon.co.uk/" + imgUrl);
-                            }
-                            else if(response && response.statusCode === 400){
-                                console.log("Bad request : https://decathlon.co.uk/" + imgUrl);
-                            }
-                            else{
-                                console.log("Image OK : https://decathlon.co.uk/" + imgUrl);
-                            }
-                        }
-                        else {
-                            console.log(error);
-                        }
+                setTimeout(function(){
+                    testImgStatus(imgUrl);
+                }, 1500);
 
-                    });
             });
         });
     }
